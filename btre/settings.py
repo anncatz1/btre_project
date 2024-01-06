8d4f7b5c2116b5db5f0e1d1da18ef9cb796c962c
Annie <anniexu2001@gmail.com>
2024-01-05 20:47:52 -0600

    change

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
