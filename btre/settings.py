75612520929ef62eada6a54656e89776c0f8437d
Annie <anniexu2001@gmail.com>
2024-01-05 20:37:17 -0600

    requirements

--- btre/settings.py
+++ btre/settings.py
@@ -140,11 +140,11 @@ MESSAGE_TAGS = {
 }
 
 # email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True 
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER=''
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_USE_TLS=True 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
